2abdee5eac211f803539e2f069007697697b0399
remmie.vail <remmie.vail@signanthealth.com>
2021-02-23 13:10:05 -0600

    Fix hero animation back to the list from the RecipeDetails.

--- app/src/main/java/com/bracket/datasharemain/ui/main/MainFragment.kt
+++ app/src/main/java/com/bracket/datasharemain/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.Display
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -45,6 +46,9 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        postponeEnterTransition()
+        view.doOnPreDraw { startPostponedEnterTransition() }
 
         val display: Display = view.display
         val size = Point()
